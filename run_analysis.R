f2308eac513ca27f13647d762e6ceb0b70e27b7e
jotav93@gmail.com <jotav93@gmail.com>
2018-05-18 20:13:46 +0100

    Fix on column names, frequency corrected twice

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,7 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 features <- read.table("./UCI HAR Dataset/features.txt",as.is = TRUE)
 
-activity_labels <- read.table(file.path(dataPath, "activity_labels.txt"))
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 colnames(activity_labels) <- c("ActivityId", "ActivityLabel")
 
 df <- rbind(
@@ -28,10 +28,9 @@ df$activity <- factor(df$activity, levels = activity_labels[, 1]
 #step 4
 columns <- colnames(df)
 columns <- gsub("[\\(\\)-]", "", columns)
-columns <- gsub("^f", "Frequency", columns)
 columns <- gsub("^t", "Time", columns)
+columns <- gsub("^f", "Frequency", columns)
 columns <- gsub("Mag", "Magnitude", columns)
-columns <- gsub("Freq", "Frequency", columns)
 columns <- gsub("std", "StandardDeviation", columns)
 columns <- gsub("Gyro", "Gyroscope", columns)
 columns <- gsub("mean", "Mean", columns)
@@ -43,5 +42,5 @@ colnames(df) <- columns
 df_2 <- df %>% group_by(subject, activity) %>% summarise_each(funs(mean))
 
 # output to file "tidy_data.txt"
-write.table(humanActivityMeans, "tidy_data.txt", row.names = FALSE, 
+write.table(df_2, "tidy_data.txt", row.names = FALSE, 
             quote = FALSE)
